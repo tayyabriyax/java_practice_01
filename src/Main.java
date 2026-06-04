8e4f511c299950c2d8e5ecf45bf1c240552fed07
Tayyab Riaz <tayyabriyax@gmail.com>
2026-06-04 08:42:28 +0500

    Learn Type Casting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
+
+        //Explicit Type Casting
+        int num1 = 500;
+        byte num2;
+        num2 = (byte) num1;
+        System.out.println(num2);
+
+        char char1 = 'A';
+        int charUnicode;
+        charUnicode = char1;
+        System.out.println("The unicode of char 'A' is " + charUnicode);
     }
 }
