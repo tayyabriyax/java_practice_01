948ea532596e749c1aaec4ec2a1ebc75ce5120b6
Tayyab Riaz <tayyabriyax@gmail.com>
2026-07-06 20:11:33 +0500

    Learn Multithreading

---
+++ src/MultithreadingPractice.java
@@ -0,0 +1,26 @@
+public class MultithreadingPractice {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            Thread currenThread = Thread.currentThread();
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(currenThread.getName() + "'s state is " + currenThread.getState());
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            Thread currenThread = Thread.currentThread();
+
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(currenThread.getName() + "'s state is " + currenThread.getState());
+            }
+        });
+
+        // System.out.println(t1.getName() + "'s state is " + t1.getState());
+
+        t1.start();
+        t2.start();
+
+        // System.out.println(Thread.currentThread().getName() + "'s state is " + Thread.currentThread().getState());
+    }
+}
